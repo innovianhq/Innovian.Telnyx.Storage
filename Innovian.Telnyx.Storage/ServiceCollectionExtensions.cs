ef3b7bf422879bbc9d284ca2027ecf9fea36097f
Whit Waldo <whit.waldo@innovian.net>
2023-10-20 14:25:39 -0500

    Added another means of registering storage client via DI

--- Innovian.Telnyx.Storage/ServiceCollectionExtensions.cs
+++ Innovian.Telnyx.Storage/ServiceCollectionExtensions.cs
@@ -3,17 +3,28 @@
 //  -------------------------------------------------------------
 
 using Innovian.Telnyx.Storage.Services.Storage;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Innovian.Telnyx.Storage;
 
 public static class ServiceCollectionExtensions
 {
+    public static IServiceCollection AddTelynxClient(this IServiceCollection services)
+    {
+        services.Configure<TelnyxClientOptions>(options =>
+        {
+            //Set the API key with the values from IOptions<TelnyxClientOptiosn>
+            options.TelnyxApiKey = options.Value.TelnyxApiKey;
+        });
+    }
+
     /// <summary>
     /// Registers the Telnyx storage service with dependency injection.
     /// </summary>
-    /// <param name="services"></param>
-    /// <param name="setupAction"></param>
+    /// <param name="services">DI service collection instance.</param>
+    /// <param name="setupAction">Provides a setup action for obtaining options.</param>
     /// <returns></returns>
     /// <exception cref="ArgumentNullException"></exception>
     public static IServiceCollection AddTelnyxClient(this IServiceCollection services,
@@ -27,6 +38,50 @@ public static class ServiceCollectionExtensions
 
         services.AddOptions();
         services.Configure(setupAction);
+
+        return services
+            .AddHttpClient()
+            .AddSingleton<ITelnyxStorageService, TelnyxStorageService>();
+    }
+
+    /// <summary>
+    /// Registers the Telnyx storage service with dependency injection.
+    /// </summary>
+    /// <param name="services">DI service collection instance.</param>
+    /// <param name="namedConfigurationSection">The named configuration section to pull the options from.</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IServiceCollection AddTelnyxClient(this IServiceCollection services,
+        IConfiguration namedConfigurationSection)
+    {
+        if (services == null)
+            throw new ArgumentNullException(nameof(services));
+        
+        services.Configure<TelnyxClientOptions>(namedConfigurationSection);
+
+        return services
+            .AddHttpClient()
+            .AddSingleton<ITelnyxStorageService, TelnyxStorageService>();
+    }
+    
+    /// <summary>
+    /// Registers the Telnyx storage service with dependency injection.
+    /// </summary>
+    /// <param name="services">The dependency injection service collection.</param>
+    /// <param name="options">An injected instance of <see cref="IOptions{TelnyxClientOptions}"/> to perform the configuration with.</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IServiceCollection AddTelnyxClient(this IServiceCollection services,
+        IOptions<TelnyxClientOptions> options)
+    {
+        if (services == null)
+            throw new ArgumentNullException(nameof(services));
+
+        services.AddOptions<TelnyxClientOptions>()
+            .Configure(opt =>
+            {
+                opt.TelnyxApiKey = options.Value.TelnyxApiKey;
+            });
 
         return services
             .AddHttpClient()
