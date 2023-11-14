90a9ad9eff254b39411b6988e354ed9a95845559
Whit Waldo <whit.waldo@innovian.net>
2023-11-14 03:46:17 -0600

    Made bucket name validator public, added unit tests for it to match new constraints

---
+++ Innovian.Telnyx.Storage.Tests/Validation/ValidatorsTests.cs
@@ -0,0 +1,99 @@
+﻿// -------------------------------------------------------------
+// Copyright (c) 2023 Innovian Corporation. All rights reserved.
+// -------------------------------------------------------------
+
+using Innovian.Telnyx.Storage.Validation;
+
+namespace Innovian.Telnyx.Storage.Tests.Validation;
+
+[TestClass]
+public class ValidatorsTests
+{
+    [TestMethod]
+    public void CannotBeFormattedAsAnIpAddress()
+    {
+        const string name = "255.255.255.1";
+        var result = Validators.IsBucketNameValid(name);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void CannotBeTwoCharactersLong()
+    {
+        const string name = "ab";
+        var result = Validators.IsBucketNameValid(name);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void CannotExceed63CharactersLong()
+    {
+        const string name = "1234567890123456789012345678901234567890123456789012345678901234567890";
+        var result = Validators.IsBucketNameValid(name);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void MustNotContainUppercaseCharacters()
+    {
+        const string name = "ThisIsATest";
+        var result = Validators.IsBucketNameValid(name);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void MustNotContainUnderscores()
+    {
+        const string name = "This_Is_A_Test";
+        var result = Validators.IsBucketNameValid(name);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void MustNotContainADash()
+    {
+        const string name = "This-Is-A-Test";
+        var result = Validators.IsBucketNameValid(name);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void LabelsMustStartWithLowerCaseLetterOrNumber1()
+    {
+        const string name = "this.is.a.Test";
+        var result = Validators.IsBucketNameValid(name);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void LabelMustStartWithLowerCaseLetterOrNumber2()
+    {
+        const string name = "th-i0.is.a.test";
+        var result = Validators.IsBucketNameValid(name);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void LabelMustStartWithLowerCaseLetterOrNumber3()
+    {
+        const string name = "this.is.a.2est";
+        var result = Validators.IsBucketNameValid(name);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void ShouldBeValid()
+    {
+        var result = Validators.IsBucketNameValid("this-is-a.t3st");
+        Assert.IsTrue(result);
+    }
+}
--- Innovian.Telnyx.Storage/Validation/Validators.cs
+++ Innovian.Telnyx.Storage/Validation/Validators.cs
@@ -1,10 +1,15 @@
-﻿//  -------------------------------------------------------------
-//  Copyright (c) 2023 Innovian Corporation. All rights reserved.
-//  -------------------------------------------------------------
+﻿// -------------------------------------------------------------
+// Copyright (c) 2023 Innovian Corporation. All rights reserved.
+// -------------------------------------------------------------
+
+using System.Text.RegularExpressions;
 
 namespace Innovian.Telnyx.Storage.Validation;
 
-internal static class Validators
+/// <summary>
+/// Used to validate values passed into the API.
+/// </summary>
+public static class Validators
 {
     public const string BucketNameValidityMessage =
         "The bucket name must be 3-65 characters long and can consist only of lowercase letters, numbers, dots and hyphens";
@@ -16,9 +21,18 @@ internal static class Validators
     /// <returns>True if the bucket is valid; false if not.</returns>
     public static bool IsBucketNameValid(string bucketName)
     {
+        //Documentation at https://developers.telnyx.com/docs/v2/cloud_storage/endpoints/bucket_operations/createbucket/
+
         //The name must be 3 to 65 characters long.
         if (bucketName.Length is < 3 or > 65)
             return false;
+
+        //Must start with a lowercase letter or number
+        var firstCharacter = bucketName[0];
+        if (!char.IsLower(firstCharacter) && !char.IsNumber(firstCharacter))
+        {
+            return false;
+        }
 
         //The name must consist only of lowercase letters, numbers, dots (periods) or hyphens.
         foreach (var character in bucketName)
@@ -27,6 +41,57 @@ internal static class Validators
                 continue;
 
             return false;
+        }
+
+        if (bucketName.Contains('.'))
+        {
+            //Cannot be formatted as an IP address
+            var ipRegex = new Regex(@"(\d{1,3}\.){3}\d{1,3}");
+            if (ipRegex.IsMatch(bucketName))
+                return false;
+
+            var labels = bucketName.Split('.');
+            foreach (var label in labels)
+            {
+                if (!ValidateLabel(label))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// If the bucket name contains periods, this validates the label between each period.
+    /// </summary>
+    /// <param name="label">The label to validate.</param>
+    /// <returns></returns>
+    private static bool ValidateLabel(string label)
+    {
+        //Must start and end with a lowercase letter or a number - meaning it must be populated
+        if (label.Length == 0)
+            return false;
+
+        //Must start with a lowercase letter or number
+        var firstCharacter = label.First();
+        if (!char.IsNumber(firstCharacter) && !char.IsLower(firstCharacter))
+            return false;
+
+        //Must end with a lowercase letter or number
+        var lastCharacter = label.Last();
+        if (!char.IsNumber(lastCharacter) && !char.IsLower(lastCharacter))
+            return false;
+
+        
+        foreach (var character in label)
+        {
+            //Must not contain uppercase characters
+            if (char.IsUpper(character))
+                return false;
+
+            //Must not contain underscores
+            if (character == '_')
+                return false;
         }
 
         return true;
