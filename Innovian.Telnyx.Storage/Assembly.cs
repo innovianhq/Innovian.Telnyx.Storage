eadb31695d4e95a3eae1081bae134e6e34f1b4c5
Whit Waldo <whit.waldo@innovian.net>
2023-11-13 19:00:04 -0600

    Exposing internals to test project

---
+++ Innovian.Telnyx.Storage/Assembly.cs
@@ -0,0 +1,8 @@
+﻿// -------------------------------------------------------------
+// Copyright (c) 2023 Innovian Corporation. All rights reserved.
+// -------------------------------------------------------------
+
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Innovian.Telnyx.Storage.Tests")]
+namespace Innovian.Telnyx.Storage;
